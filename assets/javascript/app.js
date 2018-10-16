3e35f1e0ac741854a189de26f56533a6e700023e
Scott McAnally <scottmcanally@Scotts-MacBook-Pro.local>
2018-10-15 21:56:49 -0400

    starter bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+console.log("connected");
+console.log("The stylesheet is linked!");
+
+// $("#start").on("click", function () {
+//     console.log("I've been clicked!");
+// });
+
+// $(".theme-button").on("click", function() {
+//     audioElement.play();
+//   });
+
+function myFunction() {
+    document.getElementById("#start").style.color = "red";
+}
+
+
